be8a01d5f22be584cae92dfa839d02b37274b64f
toannvph05233 <johntoan98@gmail.com>
2022-08-03 09:44:37 +0700

    up

---
+++ src/main/java/com/example/demo_spring_security/controllers/LoginAPI.java
@@ -0,0 +1,40 @@
+package com.example.demo_spring_security.controllers;
+
+import com.example.demo_spring_security.models.AppUser;
+import com.example.demo_spring_security.services.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("*")
+public class LoginAPI {
+    @Autowired
+    JwtService jwtService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+
+    @PostMapping("/login")
+    public String login(@RequestBody AppUser appUser){
+        try {
+            // Tạo ra 1 đối tượng Authentication.
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(appUser.getUsername(), appUser.getPassword()));
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            String token = jwtService.createToken(authentication);
+            return token;
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+
+    }
+}
